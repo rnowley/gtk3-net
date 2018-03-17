3e4a38ba7d0b692e3f36e288140bc188d0f0e38f
Raymond Nowley <nowley.r@gmail.com>
2018-03-17 12:59:16 +0800

    Initial commit

---
+++ NativeMethods.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace gtk3_net
+{
+    public static class NativeMethods
+    {
+        private const string GtkLib = "libgtk-3.so.0";
+        private const string GObjLib = "libgobject-2.0.so.0";
+        
+        public delegate void GClosureNotify();
+
+        [DllImport (GtkLib, CallingConvention = CallingConvention.Cdecl,CharSet = CharSet.Ansi)]
+        public static extern IntPtr gtk_application_new(string applicationId, GApplicationFlags flags);
+        
+        [DllImport(GObjLib, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern uint g_signal_connect_data(IntPtr instance, string detiledSignal, IntPtr handler, IntPtr data, GClosureNotify destroyData, GConnectFlags connectFlags);
+        
+        [DllImport(GtkLib, CharSet = CharSet.Ansi)]
+        public static extern int g_application_run(IntPtr application, int argc, string[] argv);
+        
+        [DllImport(GtkLib)]
+        public static extern void g_object_unref(IntPtr application);
+        
+        [DllImport(GtkLib)]
+        public static extern IntPtr gtk_application_window_new(IntPtr application);
+        
+        [DllImport(GtkLib, CharSet = CharSet.Ansi)]
+        public static extern IntPtr gtk_window_set_title(IntPtr window, string title);
+        
+        [DllImport(GtkLib, CharSet = CharSet.Ansi)]
+        public static extern void gtk_window_set_default_size(IntPtr window, int width, int height);
+        
+        [DllImport(GtkLib)]
+        public static extern void gtk_widget_show_all(IntPtr widget);
+        
+        [DllImport(GtkLib)]
+        public static extern IntPtr gtk_button_box_new(GtkOrientation orientation);
+        
+        [DllImport(GtkLib)]
+        public static extern void gtk_container_add(IntPtr container, IntPtr widget);
+        
+        [DllImport(GObjLib, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern uint g_signal_connect_swapped(IntPtr instance, string detailedSignal, IntPtr handler, IntPtr data);
+        
+        [DllImport(GtkLib)]
+        public static extern void gtk_widget_destroy(IntPtr widget);
+        
+        [DllImport(GtkLib, CharSet = CharSet.Ansi)]
+        public static extern IntPtr gtk_button_new_with_label(string label);
+    }
+    
+    [Flags]
+    public enum GApplicationFlags
+    {
+        None
+    }
+    
+    [Flags]
+    public enum GConnectFlags
+    {
+        /// <summary>
+        /// whether the handler should be called before or after the default handler of the signal.
+        /// </summary>
+        ConnectAfter,
+
+        /// <summary>
+        /// whether the instance and data should be swapped when calling the handler; see g_signal_connect_swapped() for an example.
+        /// </summary>
+        ConnectSwapped
+    }
+    
+    public enum GtkOrientation
+    {
+        Horizontal,
+        Vertical
+    }
+}
---
+++ sample/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using gtk3_net;
+
+namespace sample
+{
+    public delegate void CallBack(IntPtr app, IntPtr data);
+    public delegate void CallBack2(IntPtr app);
+    
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = NativeMethods.gtk_application_new("org.gtk.example", GApplicationFlags.None);
+            NativeMethods.g_signal_connect_data(app, "activate", Marshal.GetFunctionPointerForDelegate(new CallBack(Activate)), 
+                IntPtr.Zero, FreeData, GConnectFlags.ConnectAfter);
+            NativeMethods.g_application_run(app, args.Length, args);
+            NativeMethods.g_object_unref(app);
+        }
+
+        private static void FreeData() {}
+
+        private static void Activate(IntPtr app, IntPtr data)
+        {
+            var window = NativeMethods.gtk_application_window_new(app);
+            NativeMethods.gtk_window_set_title(window, "Window");
+            NativeMethods.gtk_window_set_default_size(window, 200, 200);
+
+            var buttonBox = NativeMethods.gtk_button_box_new(GtkOrientation.Horizontal);
+            NativeMethods.gtk_container_add(window, buttonBox);
+
+            var button = NativeMethods.gtk_button_new_with_label("Hello World");
+            NativeMethods.g_signal_connect_data(button, "clicked", Marshal.GetFunctionPointerForDelegate(
+                new CallBack(PrintHello)),
+                IntPtr.Zero, FreeData, GConnectFlags.ConnectAfter);
+            // Note g_signal_connect and g_signal_connect_swapped are macros around g_signal_connect_data.
+            NativeMethods.g_signal_connect_data(button, "clicked", Marshal.GetFunctionPointerForDelegate(
+                new CallBack2(WindowDestroy)), window, FreeData, GConnectFlags.ConnectSwapped);
+            NativeMethods.gtk_container_add(buttonBox, button);
+            
+            NativeMethods.gtk_widget_show_all(window);
+        }
+        
+        private static void WindowDestroy(IntPtr app)
+        {
+            NativeMethods.gtk_widget_destroy(app);
+        }
+
+        private static void PrintHello(IntPtr app, IntPtr data)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    
+}
+
+
+//#define g_signal_connect_swapped(instance, detailed_signal, c_handler, data) \
+//g_signal_connect_data ((instance), (detailed_signal), (c_handler), (data), NULL, G_CONNECT_SWAPPED)
+
+//#define g_signal_connect(instance, detailed_signal, c_handler, data) \
+//g_signal_connect_data ((instance), (detailed_signal), (c_handler), (data), NULL, (GConnectFlags) 0)
+
+//#define g_signal_connect_after(instance, detailed_signal, c_handler, data) \
+//g_signal_connect_data ((instance), (detailed_signal), (c_handler), (data), NULL, G_CONNECT_AFTER)
+
