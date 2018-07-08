2e07de7bd8dd6c370342f7a6844e4fc36df5e767
rnowley <nowley.r@gmail.com>
2018-07-08 23:42:21 +1200

    Removed Misc because it is deprecated.

---
+++ ImageViewer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using Gtk3;
+
+namespace ImageViewer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app1 = new Application("org.gtk.example", GApplicationFlags.None);
+            app1.ConnectActivateSignal(Activate, FreeData);
+            app1.Run(0, args);
+            app1.Exit();
+        }
+
+        private static void FreeData()
+        {
+        }
+
+        private static void Activate(IntPtr app, IntPtr data)
+        {
+            var window = new Window(app) {Title = "Image Viewer"};
+
+            var box = new Box(Orientation.Vertical, 5);
+            var button = new Button("Open Image");
+            var image = new Image();
+            box.PackStart(image, true, true, 0);
+            box.PackStart(button, false, false, 0);
+
+            window.AddWidget(box);
+            window.Show();
+        }
+    }
+}
